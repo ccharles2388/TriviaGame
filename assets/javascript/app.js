696d86b8383dfc3e52401c8042e8d877996f00e3
Curtis Charles <ccharles2388@gmail.com>
2019-09-05 16:30:59 -0400

    did logic for true and false  user inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,58 @@
+
+var correct=0;
+var incorrect=0;
+var q1answer="True"
+var q2answer="False"
+var q3answer="True"
+
+
+
+
+// On Click Event T0 Submit Before Time Expires
+$("#submit").on("click", function(){
+  if($("#q1True").val()===q1answer) {
+    correct++;
+    console.log(correct);
+  }
+  else {
+    incorrect++;
+    console.log(incorrect);
+  }
+
+  if($("#q2True").val()===q1answer) {
+    correct++;
+    console.log(correct);
+  }
+  else {
+    incorrect++;
+    console.log(incorrect);
+  }
+  
+  if($("#q3True").val()===q1answer) {
+    correct++;
+    console.log(correct);
+  }
+  else {
+    incorrect++;
+    console.log(incorrect);
+  }
+ 
+}) 
+
 // Counter Decreases By 1 Second
-var timeLeft = 30;
-    var elem = document.getElementById('timeleft');
+// var timeLeft = 30;
+//     var elem = document.getElementById('timeleft');
     
-    var timerId = setInterval(countdown, 1000);
+//     var timerId = setInterval(countdown, 1000);
     
-    function countdown() {
-      if (timeLeft == 0) {
-        clearTimeout(timerId);
-        doSomething();
-      } else {
-        elem.innerHTML = timeLeft + ' seconds remaining';
-        timeLeft--;
-      }
-    }
+//     function countdown() {
+//       if (timeLeft == 0) {
+//         clearTimeout(timerId);
+//         doSomething();
+//       } else {
+//         elem.innerHTML = timeLeft + ' seconds remaining';
+//         timeLeft--;
+//       }
+//     }
     
 
