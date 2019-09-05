0bd8538283277a396e60ab05a288ca503bf4e15b
Curtis Charles <ccharles2388@gmail.com>
2019-09-05 00:36:27 -0400

    create counter

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+// Counter Decreases By 1 Second
+var timeLeft = 30;
+    var elem = document.getElementById('timeleft');
+    
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (timeLeft == 0) {
+        clearTimeout(timerId);
+        doSomething();
+      } else {
+        elem.innerHTML = timeLeft + ' seconds remaining';
+        timeLeft--;
+      }
+    }
+
